266824de53897ebbb14f35bf343201267288da5f
YOOBINNOH <kuninyb@naver.com>
2022-09-16 17:50:57 +0900

    update RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -50,19 +51,31 @@ public class BasicItemController {
 //
 //    }
 
+//    @PostMapping("/add")
+//    public String save2(@ModelAttribute("item") Item item,Model model){
+//
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item",item);
+//
+//        return "basic/item";
+//
+//    }
+
     @PostMapping("/add")
-    public String save2(@ModelAttribute("item") Item item,Model model){
+    public String save3(Item item, RedirectAttributes redirectAttributes){
+
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
 
 
-        itemRepository.save(item);
 
-        model.addAttribute("item",item);
-
-        return "basic/item";
+        return "redirect:/basic/items/{itemId}";
 
     }
-
-
 
 
     @PostConstruct
